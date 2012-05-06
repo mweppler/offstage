207a1a73f1383f14cb343dbc3a1b4b180d87e296
Matt Weppler <mweppler@gmail.com>
2012-05-05 18:37:27 -0700

    - committing empty project

---
+++ Offstage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Offstage
+//
+//  Created by matt weppler on 5/5/12.
+//  Copyright (c) 2012 matt weppler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Offstage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Offstage
+//
+//  Created by matt weppler on 5/5/12.
+//  Copyright (c) 2012 matt weppler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
